a2d3eb6bb4248e8d78fab54aad8d5c18c6cc8c0f
Shogo Wada <shogowada@users.noreply.github.com>
2021-12-02 21:40:35 -0800

    Extract createRating function

---
+++ src/03/create-rating.ts
@@ -0,0 +1,30 @@
+import { intRange } from "../common";
+
+export const createRating = (rows: string[], majorityDigit: number): number => {
+  const digitCount: number = rows[0].length;
+
+  const ratingAsString: string = intRange(digitCount).reduce(
+    createRowsReducer(majorityDigit),
+    rows
+  )[0];
+
+  return parseInt(ratingAsString, 2);
+};
+
+const createRowsReducer =
+  (majorityDigit: number) =>
+  (rows: string[], index: number): string[] => {
+    if (rows.length === 1) {
+      return rows;
+    }
+
+    const oneCount: number = rows.filter((row) => row[index] === `1`).length;
+
+    if (oneCount >= rows.length / 2) {
+      return rows.filter((row) => row[index] === `${majorityDigit}`);
+    } else {
+      return rows.filter(
+        (row) => row[index] === `${majorityDigit === 0 ? 1 : 0}`
+      );
+    }
+  };
--- src/03/index.ts
+++ src/03/index.ts
@@ -1,12 +1,11 @@
 import * as path from "path";
-import { intRange, readTextFileRowsSync } from "../common";
+import { readTextFileRowsSync } from "../common";
 import { createRate } from "./create-rate";
+import { createRating } from "./create-rating";
 
 const inputRows: string[] = readTextFileRowsSync(
   path.join(__dirname, "input.txt")
 );
-
-const digitCount: number = inputRows[0].length;
 
 const gammaRate: number = createRate(inputRows, 1);
 const epsilonRate: number = createRate(inputRows, 0);
@@ -15,51 +14,9 @@ console.log(`gammaRate: ${gammaRate}`);
 console.log(`epsilonRate: ${epsilonRate}`);
 console.log(`powerConsumption: ${gammaRate * epsilonRate}`);
 
-const oxygenGeneratorRatingAsString: string = intRange(digitCount).reduce(
-  (rows: string[], index: number): string[] => {
-    if (rows.length === 1) {
-      return rows;
-    }
+const oxygenGeneratorRating: number = createRating(inputRows, 1);
+const co2ScrubberRating: number = createRating(inputRows, 0);
 
-    const oneCount: number = rows.filter((row) => row[index] === "1").length;
-
-    if (oneCount >= rows.length / 2) {
-      return rows.filter((row) => row[index] === "1");
-    } else {
-      return rows.filter((row) => row[index] === "0");
-    }
-  },
-  inputRows
-)[0];
-
-const oxygenGeneratorRating: number = parseInt(
-  oxygenGeneratorRatingAsString,
-  2
-);
-
-console.log(`oxygenGeneratorRatingAsString: ${oxygenGeneratorRatingAsString}`);
-console.log(`oxygenGeneratorRating: ${oxygenGeneratorRating}`);
-
-const co2ScrubberRatingAsString: string = intRange(digitCount).reduce(
-  (rows: string[], index: number): string[] => {
-    if (rows.length === 1) {
-      return rows;
-    }
-
-    const oneCount: number = rows.filter((row) => row[index] === "1").length;
-
-    if (oneCount >= rows.length / 2) {
-      return rows.filter((row) => row[index] === "0");
-    } else {
-      return rows.filter((row) => row[index] === "1");
-    }
-  },
-  inputRows
-)[0];
-
-const co2ScrubberRating: number = parseInt(co2ScrubberRatingAsString, 2);
-
-console.log(`co2ScrubberRatingAsString: ${co2ScrubberRatingAsString}`);
-console.log(`co2ScrubberRating: ${co2ScrubberRating}`);
-
-console.log(co2ScrubberRating * oxygenGeneratorRating);
+console.log(`oxygenGeneratorRating: ${oxygenGeneratorRating}`); // 1327
+console.log(`co2ScrubberRating: ${co2ScrubberRating}`); // 3429
+console.log(`lifeSupportRating: ${co2ScrubberRating * oxygenGeneratorRating}`); // 4550283
