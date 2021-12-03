8e687a8093eaf827100771a70f0a11c5ad31187e
Shogo Wada <shogowada@users.noreply.github.com>
2021-12-02 21:25:48 -0800

    Complete day 3 part 2

--- src/03/index.ts
+++ src/03/index.ts
@@ -53,3 +53,52 @@ console.log(`trueCounts: ${trueCounts}`);
 console.log(`gammaRate: ${gammaRate}`);
 console.log(`epsilonRate: ${epsilonRate}`);
 console.log(`powerConsumption: ${gammaRate * epsilonRate}`);
+
+const oxygenGeneratorRatingAsString: string = intRange(digitCount).reduce(
+  (rows: string[], index: number): string[] => {
+    if (rows.length === 1) {
+      return rows;
+    }
+
+    const oneCount: number = rows.filter((row) => row[index] === "1").length;
+
+    if (oneCount >= rows.length / 2) {
+      return rows.filter((row) => row[index] === "1");
+    } else {
+      return rows.filter((row) => row[index] === "0");
+    }
+  },
+  inputRows
+)[0];
+
+const oxygenGeneratorRating: number = parseInt(
+  oxygenGeneratorRatingAsString,
+  2
+);
+
+console.log(`oxygenGeneratorRatingAsString: ${oxygenGeneratorRatingAsString}`);
+console.log(`oxygenGeneratorRating: ${oxygenGeneratorRating}`);
+
+const co2ScrubberRatingAsString: string = intRange(digitCount).reduce(
+  (rows: string[], index: number): string[] => {
+    if (rows.length === 1) {
+      return rows;
+    }
+
+    const oneCount: number = rows.filter((row) => row[index] === "1").length;
+
+    if (oneCount >= rows.length / 2) {
+      return rows.filter((row) => row[index] === "0");
+    } else {
+      return rows.filter((row) => row[index] === "1");
+    }
+  },
+  inputRows
+)[0];
+
+const co2ScrubberRating: number = parseInt(co2ScrubberRatingAsString, 2);
+
+console.log(`co2ScrubberRatingAsString: ${co2ScrubberRatingAsString}`);
+console.log(`co2ScrubberRating: ${co2ScrubberRating}`);
+
+console.log(co2ScrubberRating * oxygenGeneratorRating);
