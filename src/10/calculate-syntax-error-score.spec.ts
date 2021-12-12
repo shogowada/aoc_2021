9da937d8c38b399117439faad337c5207c82c18f
Shogo Wada <shogowada@users.noreply.github.com>
2021-12-12 14:52:42 -0600

    Complete day 10 part 2

--- src/10/calculate-syntax-error-score.spec.ts
+++ src/10/calculate-syntax-error-score.spec.ts
@@ -2,21 +2,46 @@ import * as path from "path";
 import { describe, beforeEach, it } from "mocha";
 import { expect } from "chai";
 import { readTextFileRowsSync } from "../common";
-import { calculateSyntaxErrorScore } from "./calculate-syntax-error-score";
+import {
+  calculateAutoCompleteScore,
+  calculateSyntaxErrorScore,
+} from "./calculate-syntax-error-score";
 
 describe("day 10: calculate-syntax-error-score", () => {
   [
-    { inputFileName: "input.test.txt", expectedScore: 26397 },
-    { inputFileName: "input.txt", expectedScore: 366027 },
-  ].forEach(({ inputFileName, expectedScore }) => {
-    let rows: string[];
+    {
+      inputFileName: "input.test.txt",
+      expectedSyntaxErrorScore: 26397,
+      expectedAutoCompleteScore: 288957,
+    },
+    {
+      inputFileName: "input.txt",
+      expectedSyntaxErrorScore: 366027,
+      expectedAutoCompleteScore: 1118645287,
+    },
+  ].forEach(
+    ({
+      inputFileName,
+      expectedSyntaxErrorScore,
+      expectedAutoCompleteScore,
+    }) => {
+      let rows: string[];
 
-    beforeEach(() => {
-      rows = readTextFileRowsSync(path.join(__dirname, inputFileName));
-    });
+      beforeEach(() => {
+        rows = readTextFileRowsSync(path.join(__dirname, inputFileName));
+      });
 
-    it(`should calculate syntax error score for ${inputFileName}`, () => {
-      expect(calculateSyntaxErrorScore(rows)).to.equal(expectedScore);
-    });
-  });
+      it(`should calculate syntax error score for ${inputFileName}`, () => {
+        expect(calculateSyntaxErrorScore(rows)).to.equal(
+          expectedSyntaxErrorScore
+        );
+      });
+
+      it(`should calculate auto complete score for ${inputFileName}`, () => {
+        expect(calculateAutoCompleteScore(rows)).to.equal(
+          expectedAutoCompleteScore
+        );
+      });
+    }
+  );
 });
--- src/10/calculate-syntax-error-score.ts
+++ src/10/calculate-syntax-error-score.ts
@@ -4,7 +4,16 @@ const ClosingCharacters: string[] = [")", "]", "}", ">"];
 interface ClosingCharacterMetadata {
   openingCharacter: string;
   syntaxErrorScore: number;
+  autoCompleteScore: number;
 }
+
+const mapOpeningCharacterToScore = (openingCharacter: string): number => {
+  const metadata = ClosingCharacters.map((closingCharacter) =>
+    mapClosingCharacterToMetadata(closingCharacter)
+  ).find((metadata) => metadata.openingCharacter === openingCharacter)!;
+
+  return metadata.autoCompleteScore;
+};
 
 const mapClosingCharacterToMetadata = (
   closingCharacter: string
@@ -14,16 +23,29 @@ const mapClosingCharacterToMetadata = (
       return {
         openingCharacter: "(",
         syntaxErrorScore: 3,
+        autoCompleteScore: 1,
       };
     }
     case "]": {
-      return { openingCharacter: "[", syntaxErrorScore: 57 };
+      return {
+        openingCharacter: "[",
+        syntaxErrorScore: 57,
+        autoCompleteScore: 2,
+      };
     }
     case "}": {
-      return { openingCharacter: "{", syntaxErrorScore: 1197 };
+      return {
+        openingCharacter: "{",
+        syntaxErrorScore: 1197,
+        autoCompleteScore: 3,
+      };
     }
     case ">": {
-      return { openingCharacter: "<", syntaxErrorScore: 25137 };
+      return {
+        openingCharacter: "<",
+        syntaxErrorScore: 25137,
+        autoCompleteScore: 4,
+      };
     }
     default: {
       throw new Error(`Unknown closing character: ${closingCharacter}`);
@@ -32,10 +54,21 @@ const mapClosingCharacterToMetadata = (
 };
 
 export const calculateSyntaxErrorScore = (rows: string[]): number => {
-  return rows.map(calculateScore).reduce((lhs, rhs) => lhs + rhs, 0);
+  return rows
+    .map(calculateSyntaxErrorScoreForRow)
+    .reduce((lhs, rhs) => lhs + rhs, 0);
 };
 
-const calculateScore = (row: string): number => {
+export const calculateAutoCompleteScore = (rows: string[]): number => {
+  const scores: number[] = rows
+    .map(calculateAutoCompleteScoreForRow)
+    .filter((score) => score)
+    .sort((lhs, rhs) => lhs - rhs);
+
+  return scores[Math.floor(scores.length / 2)];
+};
+
+const calculateSyntaxErrorScoreForRow = (row: string): number => {
   const characters: string[] = row.split("");
 
   let stack: string[] = [];
@@ -58,6 +91,29 @@ const calculateScore = (row: string): number => {
   return 0;
 };
 
-const mapClosingCharacterToScore = (character: string): number => {
-  return mapClosingCharacterToMetadata(character).syntaxErrorScore;
+const calculateAutoCompleteScoreForRow = (row: string): number => {
+  const characters: string[] = row.split("");
+
+  let stack: string[] = [];
+  for (let index = 0; index < characters.length; ++index) {
+    const character = characters[index];
+
+    if (OpeningCharacters.includes(character)) {
+      stack = [...stack, character];
+    } else {
+      const { openingCharacter } = mapClosingCharacterToMetadata(character);
+      if (stack[stack.length - 1] !== openingCharacter) {
+        return 0;
+      } else {
+        stack = stack.slice(0, stack.length - 1);
+      }
+    }
+  }
+
+  return stack.reduceRight(
+    (score: number, openingCharacter: string): number => {
+      return score * 5 + mapOpeningCharacterToScore(openingCharacter);
+    },
+    0
+  );
 };
