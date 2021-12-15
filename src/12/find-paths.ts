6b98f6a23adb074c84a4d3c83e9c2122771c3593
Shogo Wada <shogowada@users.noreply.github.com>
2021-12-15 06:50:32 -0600

    Refactor find paths

--- src/12/find-paths.ts
+++ src/12/find-paths.ts
@@ -1,5 +1,3 @@
-import { isNonNull } from "../common";
-
 interface Connection {
   a: string;
   b: string;
@@ -13,10 +11,7 @@ type Path = string[];
 export const countPaths = (rows: string[]): number => {
   const connections: Connection[] = rows.map(mapRowToConnection);
 
-  const paths: Path[] = findPaths(connections, []).filter((path) => {
-    const lastCave = path[path.length - 1];
-    return lastCave === End;
-  });
+  const paths: Path[] = findCompletePaths(connections);
 
   return paths.length;
 };
@@ -26,67 +21,77 @@ const mapRowToConnection = (row: string): Connection => {
   return { a, b };
 };
 
-const findPaths = (connections: Connection[], prevPath: Path): Path[] => {
-  if (!prevPath.length) {
-    return findPaths(connections, [Start]);
-  } else {
-    const lastCave: string = prevPath[prevPath.length - 1];
-    if (lastCave === End) {
-      return [prevPath];
-    } else if (lastCave === Start && prevPath.length !== 1) {
-      return [prevPath];
-    }
+const findCompletePaths = (connections: Connection[]): Path[] => {
+  return findPaths(connections).filter(isCompletePath);
+};
 
-    const nextCaves: string[] = findNextCaves(connections, prevPath);
-    return nextCaves.flatMap((nextCave) =>
-      findPaths(connections, [...prevPath, nextCave])
-    );
+const findPaths = (
+  connections: Connection[],
+  prevPath: Path = [Start]
+): Path[] => {
+  const lastCave: string = prevPath[prevPath.length - 1];
+  if (lastCave === End) {
+    return [prevPath];
   }
+
+  const nextCaves: string[] = findNextCaves(connections, prevPath);
+  return nextCaves.flatMap((nextCave) =>
+    findPaths(connections, [...prevPath, nextCave])
+  );
 };
 
 const findNextCaves = (connections: Connection[], prevPath: Path): string[] => {
   const lastCave = prevPath[prevPath.length - 1];
 
-  const visitedSmallCaves: string[] = prevPath.filter(isSmallCave);
-
-  const smallCaveToVisitedCountDictionary: Record<string, number> = prevPath
-    .filter(isSmallCave)
-    .reduce(
-      (
-        smallCaveToVisitedCountDictionary: Record<string, number>,
-        smallCave: string
-      ) => {
-        return {
-          ...smallCaveToVisitedCountDictionary,
-          [smallCave]: (smallCaveToVisitedCountDictionary[smallCave] || 0) + 1,
-        };
-      },
-      {}
-    );
+  const smallCaveToVisitedCountDictionary: Record<string, number> =
+    mapPathToSmallCaveToVisitedCountDictionary(prevPath);
 
   const hasVisitedAnySmallCaveTwice: boolean = Object.values(
     smallCaveToVisitedCountDictionary
   ).some((count) => count >= 2);
 
-  return connections
-    .map((connection) => {
-      if (connection.a === lastCave) {
-        return connection.b;
-      } else if (connection.b === lastCave) {
-        return connection.a;
-      } else {
-        return null;
-      }
-    })
-    .filter(isNonNull)
-    .filter((cave) => {
-      if (hasVisitedAnySmallCaveTwice) {
-        return !visitedSmallCaves.includes(cave);
-      } else {
-        return true;
-      }
-    });
+  const mapConnectionToNextCave = (connection: Connection): string | null => {
+    if (connection.a === lastCave) {
+      return connection.b;
+    } else if (connection.b === lastCave) {
+      return connection.a;
+    } else {
+      return null;
+    }
+  };
+
+  const canVisit = (cave: string | null): cave is string => {
+    if (!cave || cave === Start) {
+      return false;
+    } else if (hasVisitedAnySmallCaveTwice) {
+      return !smallCaveToVisitedCountDictionary[cave];
+    } else {
+      return true;
+    }
+  };
+
+  return connections.map(mapConnectionToNextCave).filter(canVisit);
+};
+
+const mapPathToSmallCaveToVisitedCountDictionary = (
+  path: Path
+): Record<string, number> => {
+  return path.filter(isSmallCave).reduce(
+    (
+      smallCaveToVisitedCountDictionary: Record<string, number>,
+      smallCave: string
+    ) => ({
+      ...smallCaveToVisitedCountDictionary,
+      [smallCave]: (smallCaveToVisitedCountDictionary[smallCave] || 0) + 1,
+    }),
+    {}
+  );
 };
 
 const isSmallCave = (cave: string): cave is Lowercase<string> =>
   cave.toLowerCase() === cave;
+
+const isCompletePath = (path: Path): boolean => {
+  const lastCave: string = path[path.length - 1];
+  return lastCave === End;
+};
